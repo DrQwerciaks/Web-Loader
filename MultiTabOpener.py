c3cb831a451580c9ed4e06424f23853355e0e54d
DrQwerciak <77661984+DrQwerciaks@users.noreply.github.com>
2023-10-28 13:57:50 +0200

    Update MultiTabOpener.py

--- MultiTabOpener.py
+++ MultiTabOpener.py
@@ -1,4 +1,15 @@
 import webbrowser
+from colorama import init, Fore
+
+init(autoreset=True)
+
+def print_ascii_art():
+    print(Fore.RED + '________        ________                              .__        __ ')
+    print(Fore.RED + '\\______ \\_______\\_____  \\__  _  __ ___________   ____ |__|____  |  | __ ')
+    print(Fore.RED + "|    |  \\_  __ \\/  / \\  \\ \\/ \\/ // __ \\_  __ \\_/ ___\\|  \\__  \\ |  |/ / ")
+    print(Fore.RED + "|    `   \\  | \\/   \\_/.  \\     /\\  ___/|  | \\/\\  \\___|  |/ __ \\|    <")
+    print(Fore.RED + "/_______  /__|  \\_____\\ \\_/\\/\\_/  \\___  >__|    \\___  >__(____  /__|_ \\ ")
+    print(Fore.RED + "        \\/             \\__>           \\/            \\/        \\/     \\/")
 
 def open_multiple_tabs(url, tab_count):
     try:
@@ -8,6 +19,7 @@ def open_multiple_tabs(url, tab_count):
         print("Nie udało się otworzyć przeglądarki lub karty na wskazany adres. Sprawdź, czy adres URL jest prawidłowy.")
 
 def main():
+    print_ascii_art()
     while True:
         url = input("Podaj adres strony: ")
         try:
