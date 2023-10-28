0899a33c31012f7647bbe60f72fba7df9f87bb97
DrQwerciak <77661984+DrQwerciaks@users.noreply.github.com>
2023-10-28 14:54:02 +0200

    Update
    Dodano funkcje save_urls i load_urls do obsługi zapisywania i wczytywania URL-i.
    Użytkownik może teraz wybrać między wprowadzeniem nowego URL-a a wyborem z zapisanych.
    Zapisane URL-e są przechowywane w pliku saved_urls.txt.

--- MultiTabOpener.py
+++ MultiTabOpener.py
@@ -1,5 +1,6 @@
 import webbrowser
 from colorama import init, Fore
+import os
 
 init(autoreset=True)
 
@@ -19,28 +20,58 @@ def print_ascii_art():
     print(gray + "\\____|__  /____/|____/__| |__| |____|  (____  /___  /\\_______  /   __/ \\___  >___|  /\\___  >__|")
     print(gray + "        \\/                                  \\/    \\/         \\/|__|        \\/     \\/     \\/")
 
-def open_multiple_tabs(url, tab_count):
-    try:
-        for _ in range(tab_count):
+def save_urls(urls):
+    with open("saved_urls.txt", "w") as file:
+        for url in urls:
+            file.write(url + "\n")
+
+def load_urls():
+    if not os.path.exists("saved_urls.txt"):
+        return []
+    with open("saved_urls.txt", "r") as file:
+        return [url.strip() for url in file.readlines()]
+
+def open_multiple_tabs(urls):
+    for url in urls:
+        try:
             webbrowser.open_new_tab(url)
-    except webbrowser.Error:
-        print("Nie udało się otworzyć przeglądarki lub karty na wskazany adres. Sprawdź, czy adres URL jest prawidłowy.")
+        except webbrowser.Error:
+            print("Nie udało się otworzyć przeglądarki lub karty na wskazany adres: " + url)
 
 def main():
     print_ascii_art()
-    while True:
-        url = input("Podaj adres strony: ")
-        try:
-            tab_count = int(input("Podaj liczbę Kart: "))
-            open_multiple_tabs(url, tab_count)
-            print("Ukończono ładowanie stron.")
-        except ValueError:
-            print("Podano nieprawidłową liczbę kart.")
+    saved_urls = load_urls()
 
-        repeat = input("Czy chcesz powtórzyć działanie? (Y/N): ").upper()
-        if repeat != "Y":
+    while True:
+        print("1. Wpisz nowy URL")
+        print("2. Wybierz z zapisanych URL-i")
+        choice = input("Wybierz opcję (1/2): ")
+
+        urls_to_open = []
+        if choice == "1":
+            url = input("Podaj adres strony: ")
+            urls_to_open.append(url)
+            if input("Czy zapisać ten URL? (Y/N): ").upper() == "Y":
+                saved_urls.append(url)
+                save_urls(saved_urls)
+        elif choice == "2" and saved_urls:
+            for idx, url in enumerate(saved_urls):
+                print(f"{idx + 1}. {url}")
+            index = int(input("Wybierz numer URL-a: ")) - 1
+            urls_to_open.append(saved_urls[index])
+        else:
+            print("Nie ma zapisanych URL-i lub nieprawidłowy wybór.")
+
+        if urls_to_open:
+            try:
+                tab_count = int(input("Podaj liczbę Kart: "))
+                open_multiple_tabs(urls_to_open * tab_count)
+                print("Ukończono ładowanie stron.")
+            except ValueError:
+                print("Podano nieprawidłową liczbę kart.")
+
+        if input("Czy chcesz powtórzyć działanie? (Y/N): ").upper() != "Y":
             break
 
 if __name__ == "__main__":
     main()
-
